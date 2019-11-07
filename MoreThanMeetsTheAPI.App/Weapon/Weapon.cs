27f58a40338e6a1fd7457fb4489c8d329e56af07
Garrett van Wageningen <ghvanw283@gmail.com>
2019-11-06 22:20:30 -0600

    change to nullable fields

--- MoreThanMeetsTheAPI.App/Weapon/Weapon.cs
+++ MoreThanMeetsTheAPI.App/Weapon/Weapon.cs
@@ -5,9 +5,9 @@ using System.Text;
 namespace MoreThanMeetsTheAPI.Weapon {
     public class Weapon {
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public IEnumerable<Transformer.Transformer> transformers { get; set; } = new List<Transformer.Transformer>();
-        public DateTime Created { get; set; }
-        public string url { get; set; }
+        public DateTime? Created { get; set; }
+        public string? url { get; set; }
     }
 }
